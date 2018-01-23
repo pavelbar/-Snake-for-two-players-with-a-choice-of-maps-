54c020ad8ffc96e16ea43009ea645e94e14b8be8
Baranov Pavel <pavelbaranow@gmail.com>
2018-01-23 20:03:34 +0300

    raw coordinate array

--- graph.cpp
+++ graph.cpp
@@ -48,13 +48,14 @@ void Symbol_to_point(int X, int Y, char symbol, ConsoleColor FOREGROUND, Console
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (WORD)((Black << 4) | White));
 }
 
-void Coordinate_transformation_by_button(int code, int &X, int &Y, int &delta_x, int &delta_y)
+void Coordinate_transformation_by_button(int code, int *X, int *Y, int len, int &delta_x, int &delta_y)
 {
 	switch (code)
 	{
 	case 72://up 
 	{
-		Y--;
+		for (int i = 0; i < len; i++)
+		Y[i]--;
 		delta_x = 0;
 		delta_y = -1;
 	}
@@ -62,7 +63,8 @@ void Coordinate_transformation_by_button(int code, int &X, int &Y, int &delta_x,
 
 	case 80://down 
 	{
-		Y++;
+		for (int i = 0; i < len; i++)
+		Y[i]++;
 		delta_x = 0;
 		delta_y = 1;
 	}
@@ -70,7 +72,8 @@ void Coordinate_transformation_by_button(int code, int &X, int &Y, int &delta_x,
 
 	case 75://left 
 	{
-		X--;
+		for (int i = 0; i < len; i++)
+		X[i]--;
 		delta_x = -1;
 		delta_y = 0;
 	}
@@ -78,7 +81,8 @@ void Coordinate_transformation_by_button(int code, int &X, int &Y, int &delta_x,
 
 	case 77://right 
 	{
-		X++;
+		for (int i = 0; i < len; i++)
+		X[i]++;
 		delta_x = 1;
 		delta_y = 0;
 	}
@@ -90,28 +94,34 @@ void Coordinate_transformation_by_button(int code, int &X, int &Y, int &delta_x,
 	}
 }
 
-void Coordinate_transformation_automatically(int &X, int &Y, int &delta_x, int &delta_y)
+void Coordinate_transformation_automatically(int *X, int *Y, int len, int &delta_x, int &delta_y)
 {
-	X = X + delta_x;
-	Y = Y + delta_y;
+	for (int i = 0; i < len; i++)
+	{
+		X[i] = X[i] + delta_x;
+		Y[i] = Y[i] + delta_y;
+	}
 }
 
-void Correction_of_oordinates(int &X, int &Y)
+void Correction_of_oordinates(int *X, int *Y, int len)
 {
-	//zone up (1-77;0)
-	if ((X >= 1) && (X <= (MAX_X - 2)) && (Y == 0)) Y = (MAX_Y - 2);
+	for (int i = 0; i < len; i++)
+	{
+		//zone up (1-77;0)
+		if ((X[i] >= 1) && (X[i] <= (MAX_X - 2)) && (Y[i] == 0)) Y[i] = (MAX_Y - 2);
 
-	//zone right (78;1-22)
-	if ((X == (MAX_X - 1)) && (Y >= 1) && (Y <= (MAX_Y - 2))) X = 1;
+		//zone right (78;1-22)
+		if ((X[i] == (MAX_X - 1)) && (Y[i] >= 1) && (Y[i] <= (MAX_Y - 2))) X[i] = 1;
 
-	//zone down (1-77;23)
-	if ((X >= 1) && (X <= (MAX_X - 2)) && (Y == (MAX_Y - 1))) Y = 1;
+		//zone down (1-77;23)
+		if ((X[i] >= 1) && (X[i] <= (MAX_X - 2)) && (Y[i] == (MAX_Y - 1))) Y[i] = 1;
 
-	//zone left (0;1-22)
-	if ((X == 0) && (Y >= 1) && (Y <= (MAX_Y - 2))) X = (MAX_X - 2);
+		//zone left (0;1-22)
+		if ((X[i] == 0) && (Y[i] >= 1) && (Y[i] <= (MAX_Y - 2))) X[i] = (MAX_X - 2);
+	}
 }
 
-void Generation_of_food_coordinates(int &food_x, int &food_y, int X, int Y)
+void Generation_of_food_coordinates(int &food_x, int &food_y, int *X, int *Y, int len)
 {
 	random_device random_device;
 	mt19937 generator(random_device());
@@ -119,21 +129,60 @@ void Generation_of_food_coordinates(int &food_x, int &food_y, int X, int Y)
 	uniform_int_distribution<int> gen_Y(1, (MAX_Y - 2)); // Равномерное распределение [a, c]
 	int rnd_X = gen_X(generator);
 	int rnd_Y = gen_Y(generator);
-	while ((rnd_X == food_x) || (rnd_X == X))
+	
+	bool stop = true;
+
+	while (stop)
+	{
 		rnd_X = gen_X(generator);
 
-	while ((rnd_Y == food_y) || (rnd_Y == Y))
+		int sum_tmp = 0;
+		for (int i = 0; i < len; i++)
+			if (rnd_X != X[i]) sum_tmp++;
+
+		if ((rnd_X != food_x) && (sum_tmp == len)) stop = false;
+	}
+
+	stop = true;
+
+	while (stop)
+	{
 		rnd_Y = gen_Y(generator);
+
+		int sum_tmp = 0;
+		for (int i = 0; i < len; i++)
+			if (rnd_Y != Y[i]) sum_tmp++;
+
+		if ((rnd_Y != food_y) && (sum_tmp == len)) stop = false;
+	}
 
 	food_x = rnd_X;
 	food_y = rnd_Y;
 }
 
+void Clear_snake(int *X, int *Y, int len, ConsoleColor FOREGROUND, ConsoleColor BACKGROUND)
+{
+	for (int i = 0; i <len; i++)
+		Symbol_to_point(X[i], Y[i], '\0', FOREGROUND, BACKGROUND);
+}
+
+void Print_snake(int *X, int *Y, int len, ConsoleColor FOREGROUND, ConsoleColor BACKGROUND)
+{
+	for (int i = 0; i <len; i++)
+		Symbol_to_point(X[i], Y[i], '*', FOREGROUND, BACKGROUND);
+}
+
 int main()
 {
-	int X = int(MAX_X / 2), Y = int(MAX_Y / 2);//Начальное положение точки
+	int *p_X_coordinate_of_snake = new int[MAX_X * MAX_Y];//массив координат Х змейки
+	int *p_Y_coordinate_of_snake = new int[MAX_X * MAX_Y];//массив координат У змейки
+	int length_of_snake = 1;//длина змейки
+
+	p_X_coordinate_of_snake[0] = int(MAX_X / 2);//Начальное положение змейки
+	p_Y_coordinate_of_snake[0] = int(MAX_Y / 2);//Начальное положение змейки
+	///int X = int(MAX_X / 2), Y = int(MAX_Y / 2);//Начальное положение точки
 	int delta_x = 0, delta_y = 0;//Предыдущее изменение позиции
-	int food_x = X, food_y = Y;//Координаты положения еды
+	int food_x = int(MAX_X / 2), food_y = int(MAX_Y / 2);//Координаты положения еды
 	bool need_food = 1;
 
 	Graph_border(MAX_X, MAX_Y);//start
@@ -143,21 +192,23 @@ int main()
 		if (need_food == 1)
 		{
 			Symbol_to_point(food_x, food_y, '\0', FOREGROUND, BACKGROUND);
-			Generation_of_food_coordinates(food_x, food_y, X, Y);
+			Generation_of_food_coordinates(food_x, food_y, p_X_coordinate_of_snake, p_Y_coordinate_of_snake, length_of_snake);
 			Symbol_to_point(food_x, food_y, 'F', FOREGROUND, BACKGROUND);
 			need_food = 0;
 		}
 
-		Symbol_to_point(X, Y, '\0', FOREGROUND, BACKGROUND);
-		if (_kbhit() != 0) Coordinate_transformation_by_button(_getch(), X, Y, delta_x, delta_y);//если кнопка нажата
+		Clear_snake(p_X_coordinate_of_snake, p_Y_coordinate_of_snake, length_of_snake, FOREGROUND, BACKGROUND);
+		if (_kbhit() != 0) Coordinate_transformation_by_button(_getch(), p_X_coordinate_of_snake, p_Y_coordinate_of_snake, length_of_snake, delta_x, delta_y);//если кнопка нажата
 		else
-			Coordinate_transformation_automatically(X, Y, delta_x, delta_y);//если кнопка не нажата
+			Coordinate_transformation_automatically(p_X_coordinate_of_snake, p_Y_coordinate_of_snake, length_of_snake, delta_x, delta_y);//если кнопка не нажата
 
-		Correction_of_oordinates(X, Y);
-		Symbol_to_point(X, Y, '*', FOREGROUND, BACKGROUND);
+		Correction_of_oordinates(p_X_coordinate_of_snake, p_Y_coordinate_of_snake, length_of_snake);
+		Print_snake(p_X_coordinate_of_snake, p_Y_coordinate_of_snake, length_of_snake, FOREGROUND, BACKGROUND);
 
 		Sleep(START_DELAY);
 	}
 
+	delete[] p_X_coordinate_of_snake;
+	delete[] p_Y_coordinate_of_snake;
 	return 0;
 }
